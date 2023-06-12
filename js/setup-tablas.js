f415bb72b5557e89655d75a090967c944f54944f
AlanSherar <112828445+AlanSherar@users.noreply.github.com>
2023-06-12 05:32:49 -0300

    mejorada la logica

--- js/setup-tablas.js
+++ js/setup-tablas.js
@@ -1,59 +1,80 @@
 document.addEventListener("DOMContentLoaded", function(){
   "use strict"
   
-  const microfonos = [{
-    "nombre": "AT2020",
-    "patron": "Cardioide",
-    "rango": "20Hz - 20kHz",
-    "nivelPS": "144dB"
-  },
-  {
-    "nombre": "AKG P120",
-    "patron": "Cardioide",
-    "rango": "20Hz - 20kHz",
-    "nivelPS": "130/150dB"
-  },
-  {
-    "nombre": "Behringer C-1",
-    "patron": "Cardioide",
-    "rango": "40Hz - 20kHz",
-    "nivelPS": "136dB"
+  const microfonos = [
+    {
+      "nombre": "AT2020",
+      "patron": "Cardioide",
+      "rango": "20Hz - 20kHz",
+      "nivelPS": "144dB",
+      "best": true,
+      "item": "Micro"
+    },
+    {
+      "nombre": "AKG_P120",
+      "patron": "Cardioide",
+      "rango": "20Hz - 20kHz",
+      "nivelPS": "130/150dB",
+      "best": false,
+      "item": "Micro"
+    },
+    {
+      "nombre": "Behringer_C-1",
+      "patron": "Cardioide",
+      "rango": "40Hz - 20kHz",
+      "nivelPS": "136dB",
+      "best": false,
+      "item": "Micro"
   }];
-  const auriculares = [{
-    "nombre": "AKG K92",
-    "frecuencia": "16 Hz - 20 kHz",
-    "impedancia": "32 Ohmios",
-    "sensibilidad": "113dB"
-  },
-  {
-    "nombre": "AKG K175",
-    "frecuencia": "18 Hz - 26 kHz",
-    "impedancia": "32 Ohmios",
-    "sensibilidad": "114dB"
-  },
-  {
-    "nombre": "ATH M20X",
-    "frecuencia": "15 Hz - 20 kHz",
-    "impedancia": "47 Ohmios",
-    "sensibilidad": "96dB"
+  const auriculares = [
+    {
+      "nombre": "AKG_K92",
+      "frecuencia": "16 Hz - 20 kHz",
+      "impedancia": "32 Ohmios",
+      "sensibilidad": "113dB",
+      "best": true,
+      "item": "Auri"
+    },
+    {
+      "nombre": "AKG_K175",
+      "frecuencia": "18 Hz - 26 kHz",
+      "impedancia": "32 Ohmios",
+      "sensibilidad": "114dB",
+      "best": false,
+      "item": "Auri"
+    },
+    {
+      "nombre": "ATH_M20X",
+      "frecuencia": "15 Hz - 20 kHz",
+      "impedancia": "47 Ohmios",
+      "sensibilidad": "96dB",
+      "best": false,
+      "item": "Auri"
   }];
-  const placas = [{
-    "nombre": "UMC404 HD",
-    "entradas": "4(+1 MIDI)",
-    "salidas": "4",
-    "monitorizacion": "Si"
-  },
-  {
-    "nombre": "UMC202 HD",
-    "entradas": "2",
-    "salidas": "2",
-    "monitorizacion": "Si"
-  },
-  {
-    "nombre": "Scarlett 2i2",
-    "entradas": "2",
-    "salidas": "2",
-    "monitorizacion": "No"
+  const placas = [
+    {
+      "nombre": "UMC404_HD",
+      "entradas": "4(+1 MIDI)",
+      "salidas": "4",
+      "monitorizacion": "Si",
+      "best": false,
+      "item": "Placa"
+    },
+    {
+      "nombre": "UMC202_HD",
+      "entradas": "2",
+      "salidas": "2",
+      "monitorizacion": "Si",
+      "best": true,
+      "item": "Placa"
+    },
+    {
+      "nombre": "Scarlett_2i2",
+      "entradas": "2",
+      "salidas": "2",
+      "monitorizacion": "No",
+      "best": false,
+      "item": "Placa"
   }];
   let setupUser = [];
   
@@ -71,47 +92,86 @@ document.addEventListener("DOMContentLoaded", function(){
   btnLimpiarSetup.addEventListener("click", vaciarSetup);
   btnSetupRandom.addEventListener("click", autoSetup);
   
-  autoSetup();
+  let tablasIDs = {
+    micros : "#tablaMicros",
+    auris : "#tablaAuris",
+    placas : "#tablaPlacas",
+    setup : "#tablaSetup"
+  }
 
+  mostrarTabla(tablasIDs.micros, microfonos);
+  mostrarTabla(tablasIDs.auris, auriculares);
+  mostrarTabla(tablasIDs.placas, placas);
+  autoSetup();
+  
   function agregar(e) {
     e.preventDefault();
     let target = e.target;
-    let tipo = target.id.split("_")[1];
+    let item = target.id.split("_")[1];
+    let placaIndex = setupUser.findIndex(elem => elem.item == "Placa");
 
+    let tipo = "";
+    switch (item){
+      case "Micro":
+        tipo = "Micrófono";
+        break;
+      case "Auri":
+        tipo = "Auricular";
+        break;
+      default:
+        tipo = "PlacaSonido";
+    }
     let formData = new FormData(target);
     let objeto = {
       "nombre": formData.get("options"),
       "tipo" : tipo,
+      "item" : item,
       "cantidad" : formData.get("cantidad")
     }
+    if(objeto.item == "Placa" && placaIndex != -1){
+      console.log("borra");
+      setupUser.splice(placaIndex,1);
+    }
     setupUser.push(objeto);
-    mostrarTabla(); 
+    mostrarTabla(tablasIDs.setup, setupUser);
   }
   function autoSetup(){
     vaciarSetup();
-    let item = microfonos[Math.floor(Math.random() * microfonos.length)];
-    setupUser.push({"nombre": item.nombre, "tipo": "Micro", "cantidad":1});
-    item = auriculares[Math.floor(Math.random() * auriculares.length)];
-    setupUser.push({"nombre": item.nombre, "tipo": "Auri", "cantidad":1});
-    item = placas[Math.floor(Math.random() * placas.length)];
-    setupUser.push({"nombre": item.nombre, "tipo": "Placa", "cantidad":1});
-    mostrarTabla();
+    let producto = microfonos[Math.floor(Math.random() * microfonos.length)];
+    setupUser.push({"nombre": producto.nombre,"tipo":"Micrófono", "item": "Micro", "cantidad":1});
+    producto = auriculares[Math.floor(Math.random() * auriculares.length)];
+    setupUser.push({"nombre": producto.nombre,"tipo":"Auricular", "item": "Auri", "cantidad":1});
+    producto = placas[Math.floor(Math.random() * placas.length)];
+    setupUser.push({"nombre": producto.nombre,"tipo":"PlacaSonido", "item": "Placa", "cantidad":1});
+    mostrarTabla(tablasIDs.setup, setupUser);
   }
-
   function vaciarSetup(){
     setupUser = [];
-    mostrarTabla();
+    mostrarTabla(tablasIDs.setup, setupUser);
   }
-  function mostrarTabla(){
-    let tb = document.querySelector("#tablaSetup tbody");
+  function mostrarTabla(tabla, array){
+    let tb = document.querySelector(`${tabla} tbody`);
     tb.innerHTML = "";
-   
-    setupUser.forEach( function(item){
+
+    array.forEach( function(producto){
       let fila = document.createElement("tr");
-      for (const key in item) {
-        let columna = document.createElement("td");
-        columna.innerHTML = item[key];
-        fila.appendChild(columna);
+      for (const key in producto) {
+        if(key != "best" && key != "item"){
+          let columna = document.createElement("td");
+          if(key == "nombre"){
+            let div = document.createElement("div");
+            div.className = "table-img";
+            let img = document.createElement("img");
+            img.src = `../imgs/setup/${producto.item}/${producto.nombre}.png`;
+            img.alt = producto[key];
+            div.appendChild(img);
+            columna.appendChild(div);
+          }
+          columna.innerHTML += producto[key];
+          fila.appendChild(columna);
+        } else if(producto[key]){
+          fila.classList.add("resaltar")
+        }
       }
       tb.appendChild(fila);
     })
